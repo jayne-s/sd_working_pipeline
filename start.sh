0d3ad8e16a2fc18d96520d88903a2de51d99800f
Jayne Srinivas <87911188+jayne-s@users.noreply.github.com>
2026-02-11 11:26:27 -0600

    Modify start.sh to include Bluetooth settings
    Updated PATH export and added Bluetooth commands.

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
-
 #!/bin/bash
 
 
 sleep 5
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/jay/nexmon/utilities
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/jay/nexmon/utilities # CHANGE THIS <-----
+bluetoothctl discoverable on
+bluetoothctl pairable on
+bluetoothctl power on
+
 
 python3 pi_server_test.py
-
-
